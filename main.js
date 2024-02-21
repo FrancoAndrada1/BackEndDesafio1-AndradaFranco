5fd49426ee3cac42e3f4a41747c646f2485b3ab5
FrancoAndrada1 <128649013+FrancoAndrada1@users.noreply.github.com>
2024-02-20 22:24:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ class ProductManager {
     addProduct(title, description, price, thumbnail, code, stock) {
         const CodigoRep = this.Products.some(product => product.code == code);
         if (CodigoRep) {
-            throw new Error(`Error el codigo esta repetido.`);
+            throw new Error("Error el codigo ya esta repetido.");
         }
         this.Products.push({id:this.productIdCounter++ , title, description, price, thumbnail, code,stock});
         
